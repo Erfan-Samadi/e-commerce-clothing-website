534f8be784690e020e6b49d90aefd78bf319fa98
Erfan Samadi <iamerfansamadi@gmail.com>
2023-08-13 15:40:05 +0330

    e-commerce website
    start of the website

---
+++ e-commerce/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import HomePage from './components/hompage'
+import './styles/App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
